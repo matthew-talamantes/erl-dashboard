9aa8fcda575dae15f9d49a95c35a7a3caf5918b9
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-11-05 20:48:17 -0400

    Create DayList

--- src/components/CalendarBody.js
+++ src/components/CalendarBody.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 const CalendarBody = ({ year, month, daysInMonth }) => {
     const firstDay = new Date(year, month, 1).getDay();
-    const dayList = [];
-    for (let i = 0; i < firstDay; i++) {
-        dayList.push({dayNum: '', events: {}});
-    }
+    const lastDay = new Date(year, month, daysInMonth).getDay();
+    const buildCalArray = () => {
+        const dayList = [];
+        for (let i = 0; i < firstDay; i++) {
+            dayList.push({dayNum: '', events: []});
+        }
+
+        for (let i = 1; i < daysInMonth; i++){
+            dayList.push({dayNum: i, events: []});
+        }
+
+        if (lastDay < 6) {
+            const trailingDays = 6 - lastDay;
+            for (let i = 0; i < trailingDays; i++){
+                dayList.push({dayNum: '', events: []});
+            }
+        }
+    
+    };
+    
 
     return (
         <tbody>
-            {firstDay}
+            {}
         </tbody>
     );
 };
