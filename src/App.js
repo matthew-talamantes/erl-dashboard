261468204727d5bef2dfa03c2f7d013d309ecd71
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-11-07 11:22:51 -0500

    Start building Events state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
 import Calendar from './components/Calendar';
 
 function App() {
+
+  const [events, setEvents] = React.useState([
+    {
+      year: 2021,
+      months: {
+        month: 0,
+        days: {
+          1: [
+            {title: 'morning shift', startTime: '8:00', endTime: '10:45', notes: ''}
+          ]
+        }
+      }
+    }
+  ]);
+
   return (
     <>
       <Header />
