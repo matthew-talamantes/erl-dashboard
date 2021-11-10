29083be7727819db0b5f56ac5b166d3cad32e7f2
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-11-10 11:31:00 -0500

    Add Test data to events state

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ import Calendar from './components/Calendar';
 
 function App() {
 
-  const [events, setEvents] = React.useState([
-    {
-      year: 2021,
-      months: {
-        month: 0,
-        days: {
-          1: [
-            {title: 'morning shift', startTime: '8:00', endTime: '10:45', notes: ''}
-          ]
-        }
+  const [events, setEvents] = React.useState({
+    2021: {
+      0: {
+        1: [
+          {
+            title: 'New Year\'s Day',
+            startTime: "0:00",
+            endTime: '23:59',
+            notes: ''
+          },
+          {
+            title: 'Test Entry',
+            startTime: '8:00',
+            endTime: '9:30',
+            notes: 'This is a test entry.'
+          }
+        ]
+      },
+      10: {
+        10: [
+          {
+            title: 'Birthday Dinner',
+            startTime: '16:30',
+            endTime: '17:30',
+            notes: 'At Ford\'s Fish Shack!'
+          }
+        ]
       }
     }
-  ]);
+  });
 
   return (
     <>
