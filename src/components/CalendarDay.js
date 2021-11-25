cb3026bfac75b95ddc9b2f3ff3a6930db8a6ea7f
Constantus <38108928+Constantus@users.noreply.github.com>
2021-11-25 17:51:25 -0500

    Start adding event popover

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -5,7 +5,7 @@ const CalendarDay = ({ dayNum, events}) => {
     return (
         <td className="calendar-day border border-dark">
             <h4>{dayNum}</h4>
-            <ul>
+            <ul className="cal-event-list">
                 {events.map((event)=>(
                     event["type"] === 'event' && <EventPreview event={event} />
                 ))}
