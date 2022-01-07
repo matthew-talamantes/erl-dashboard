c2b137714529fc495954ede707ff85a123787851
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-07 13:33:05 -0500

    Add popover placeholder

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 const EventPreview = ({ event }) => {
 
@@ -20,23 +22,38 @@ const EventPreview = ({ event }) => {
     };
 
 
-    const popoverRef = React.useRef();
-    React.useEffect(() => {
-        // var popover = new bootstrap.Popover(popoverRef.current, {
-        //     content: "Hello popover content!",
-        //     title: "My Popover"
-        // })
-        });
 
 
+    // const popoverRef = React.useRef();
+    // React.useEffect(() => {
+    //     // var popover = new bootstrap.Popover(popoverRef.current, {
+    //     //     content: "Hello popover content!",
+    //     //     title: "My Popover"
+    //     // })
+    //     });
+
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Popover Header</Popover.Header>
+            <Popover.Body>
+                This is the popover body!
+            </Popover.Body>
+        </Popover>
+    );
+
+    const EventOverlay = () => (
+        <OverlayTrigger trigger={['hover', 'focus']} placement='right' overlay={popover}>
+            <li className="cal-event ps-1">
+                <a tabIndex="0" className="" role="button" data-bs-trigger="focus">
+                    <h5 className="cal-event-title">{event['title']}</h5>
+                    <p className="cal-event-time">{getTwelveHr(event['startTime'])} - {getTwelveHr(event['endTime'])}</p>
+                </a>
+            </li>
+        </OverlayTrigger>
+    );
 
     return (
-        <li className="cal-event">
-            <a tabIndex="0" ref={popoverRef} className="btn btn-lg btn-danger" role="button" data-bs-trigger="focus">
-                <h5 className="cal-event-title">{event['title']}</h5>
-                <p className="cal-event-time">{getTwelveHr(event['startTime'])} - {getTwelveHr(event['endTime'])}</p>
-            </a>
-        </li>
+        <EventOverlay />
     );
 };
 
