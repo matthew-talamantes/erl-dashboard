4c183d1d58398e58aee7b935aa150d5d3c2c0226
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-10 20:21:32 -0500

    Start building event object

--- src/App.js
+++ src/App.js
@@ -6,39 +6,43 @@ import Calendar from './components/Calendar';
 
 function App() {
 
-  const [events, setEvents] = React.useState({
-    '2021': {
-      '0': {
-        '1': [
-          {
-            title: 'New Year\'s Day',
-            type: 'event',
-            startTime: "0:00",
-            endTime: '23:59',
-            notes: ''
+  const [events, setEvents] = React.useState({});
+
+  const buildEventsObject = rawEvents => {
+    let eventsObj = {};
+    rawEvents.forEach(event => {
+      if (event['year'] in eventsObj) {
+        // if (event['month'] in eventsObj[event['year']]) {
+        //   if (event['day'] in eventsObj[event['year']][event['month']]) {
+        //     eventsObj[event['year']][event['month']][event['day']].push(event);
+        //   } else {
+        //     eventsObj[event['year']][event['month']][event['day']] = [event];
+        //   }
+        // } else {
+        //   eventsObj[event['year']][event['month']][event['day']] = [event];
+        // }
+      } else {
+        eventsObj[event['year']] = {
+          event['month']: {
+            event['day']: [event], 
           },
-          {
-            title: 'Test Entry',
-            type: 'event',
-            startTime: '8:00',
-            endTime: '9:30',
-            notes: 'This is a test entry.'
-          }
-        ]
-      },
-      '10': {
-        '10': [
-          {
-            title: 'Birthday Dinner',
-            type: 'event',
-            startTime: '16:30',
-            endTime: '17:30',
-            notes: 'At Ford\'s Fish Shack!'
-          }
-        ]
+        };
       }
-    }
-  });
+    });
+
+    return eventsObj;
+  };
+
+  React.useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch('http://localhost:5000/events');
+      const data = await res.json();
+      const eventsObj = buildEventsObject(data);
+      console.log(eventsObj);
+    };
+
+    fetchEvents();
+  }, []);
 
   return (
     <>
