9359e135d5439d45f5965bb3e6829e315c36b8fa
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-14 20:50:33 -0500

    Create addEvent Funtion

--- src/App.js
+++ src/App.js
@@ -5,53 +5,60 @@ import Header from './components/Header';
 import Calendar from './components/Calendar';
 
 function App() {
-
+  
+  const url = 'http://localhost:5000';
   const [events, setEvents] = React.useState({});
 
-  const buildEventsObject = rawEvents => {
-    let eventsObj = {};
-    rawEvents.forEach(event => {
-      const eventYear = event['year'];
-      const eventMonth = event['month'];
-      const eventDay = event['day'];
+  const insertIntoObject= (eventsObj, event) => {
+    const eventYear = event['year'];
+    const eventMonth = event['month'];
+    const eventDay = event['day'];
 
-      if (event['year'] in eventsObj) {
-        if (eventMonth in eventsObj[eventYear]) {
-          if (eventDay in eventsObj[eventYear][eventMonth]) {
-            eventsObj[eventYear][eventMonth][eventDay].push(event);
-          } else {
-            eventsObj = {
-              [eventYear]: {
-                [eventMonth]: {
-                  [eventDay]: [event],
-                  ...eventsObj[eventYear][eventMonth]
-                },
-                ...eventsObj[eventYear]
-              },
-              ...eventsObj
-            };
-          }
+    if (event['year'] in eventsObj) {
+      if (eventMonth in eventsObj[eventYear]) {
+        if (eventDay in eventsObj[eventYear][eventMonth]) {
+          eventsObj[eventYear][eventMonth][eventDay].push(event);
         } else {
           eventsObj = {
-            ...eventsObj,
             [eventYear]: {
-              ...eventsObj[eventYear],
               [eventMonth]: {
-                [eventDay]: [event]
-              }
-            }
+                [eventDay]: [event],
+                ...eventsObj[eventYear][eventMonth]
+              },
+              ...eventsObj[eventYear]
+            },
+            ...eventsObj
           };
         }
       } else {
         eventsObj = {
           ...eventsObj,
           [eventYear]: {
+            ...eventsObj[eventYear],
             [eventMonth]: {
               [eventDay]: [event]
             }
-          },
+          }
         };
       }
+    } else {
+      eventsObj = {
+        ...eventsObj,
+        [eventYear]: {
+          [eventMonth]: {
+            [eventDay]: [event]
+          }
+        },
+      };
+    }
+
+    return eventsObj;
+  };
+
+  const buildEventsObject = rawEvents => {
+    let eventsObj = {};
+    rawEvents.forEach(event => {
+      eventsObj = insertIntoObject(eventsObj, event);
     });
 
     return eventsObj;
@@ -59,14 +66,28 @@ function App() {
 
   React.useEffect(() => {
     const fetchEvents = async () => {
-      const res = await fetch('http://localhost:5000/events');
+      const res = await fetch(`${url}/events`);
       const data = await res.json();
       const eventsObj = buildEventsObject(data);
-      console.log(eventsObj);
+      setEvents(eventsObj);
     };
 
     fetchEvents();
   }, []);
+
+  const addEvent = async (event) => {
+    const res = await fetch(`${url}/events`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(event),
+    });
+
+    const data = await res.json();
+    const newEvents = insertIntoObject(events, data);
+    setEvents(newEvents);
+  };
 
   return (
     <>
