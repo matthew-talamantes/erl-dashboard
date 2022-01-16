d3a274e63d4b314589b5ecf39f6174cde2bd6a42
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-16 10:49:15 -0500

    Add AddEvent Form and basic styling

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -2,28 +2,47 @@ import React from 'react';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const AddEvent = () => {
     return (
-        <Form>
-            <Form.Group className='mb-3' controlId='formAddEvent'>
-                <Form.Label>Event Name</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event Month</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event Day</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event Year</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event Start Time</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event End Time</Form.Label>
-                <Form.Control type='text' />
-                <Form.Label>Event Notes</Form.Label>
-                <Form.Control type='textarea' />
-            </Form.Group>
-            <Button type='submit'>Submit</Button>
-        </Form>
+        <Container>
+            <Form>
+                <Form.Group className='mb-3' controlId='formAddEvent'>
+                    <Row className='justify-content-md-center'>
+                        <Col sm md='4'>
+                            <Form.Label>Event Name</Form.Label>
+                            <Form.Control type='text' />
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-md-center'>
+                        <Col sm md='4'>
+                            <Form.Label>Event Date</Form.Label>
+                            <Form.Control type='date' />
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-md-center'>
+                        <Col sm md='2'>
+                            <Form.Label>Event Start Time</Form.Label>
+                            <Form.Control type='time' />
+                        </Col>
+                        <Col sm md='2'>
+                            <Form.Label>Event End Time</Form.Label>
+                            <Form.Control type='time' />
+                        </Col>
+                    </Row>
+                    <Row className='justify-content-md-center'>
+                        <Col sm md='4'>
+                            <Form.Label>Event Notes</Form.Label>
+                            <Form.Control type='textarea' rows={3}/>
+                        </Col>
+                    </Row>
+                </Form.Group>
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </Container>
     )
 };
 
