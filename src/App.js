97e281fa08bdb2874a619e5ad2d5b189e94e755c
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-14 11:53:27 -0500

    Intake events from django backend

--- src/App.js
+++ src/App.js
@@ -10,13 +10,44 @@ import EventView from './components/EventView';
 
 function App() {
   
-  const url = 'http://localhost:5000';
+  const url = 'http://127.0.0.1:8000';
   const [events, setEvents] = React.useState([]);
+
+  const setUtcDate = (dateStr) => {
+    const year = dateStr.slice(0, 4);
+    const month = parseInt(dateStr.slice(5, 7)) - 1;
+    const day = dateStr .slice(8, 10);
+    const hour = dateStr.slice(11, 13);
+    const minute = dateStr. slice(14, 16);
+    const second = dateStr.slice(17, 19);
+
+    const utcDate = new Date(Date.UTC(year, month, day, hour, minute, second));
+    return utcDate;
+  };
 
   React.useEffect(() => {
     const fetchEvents = async () => {
-      const res = await fetch(`${url}/events`);
-      const data = await res.json();
+      const res = await fetch(`${url}/events/`);
+      const jsonData = await res.json();
+      // Convert to expected format
+      const data = jsonData.map(item => {
+        let eventObj = {};
+        eventObj['id'] = item['uid'];
+        eventObj['title'] = item['title'];
+        eventObj['notes'] = item['description'];
+        eventObj['type'] = item['eventType'];
+        
+        // For now you can't have a endTime on a different day as startTime
+        const utcStart = setUtcDate(item['startTime']);
+        const utcEnd = setUtcDate(item['endTime']);
+        eventObj['year'] = utcStart.getFullYear();
+        eventObj['month'] = utcStart.getMonth();
+        eventObj['day'] = utcStart.getDate();
+        eventObj['startTime'] = `${utcStart.getHours()}:${utcStart.getMinutes()}`;
+        eventObj['endTime'] = `${utcEnd.getHours()}:${utcEnd.getMinutes()}`;
+        return eventObj;
+      });
+
       setEvents(data);
     };
 
