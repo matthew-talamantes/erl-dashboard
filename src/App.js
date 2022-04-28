2add721471d0909d8e815a5681fb9ca67c8f77b6
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-04-28 16:30:03 -0400

    Fix 404 error bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import Header from './components/Header';
-import AddEvent from './components/AddEvent';
-import Calendar from './components/Calendar';
-import EventView from './components/EventView';
+import Header from "./components/Header";
+import AddEvent from "./components/AddEvent";
+import Calendar from "./components/Calendar";
+import EventView from "./components/EventView";
 
 function App() {
-  
-  const url = 'http://127.0.0.1:8000';
+  const url = "http://127.0.0.1:8000";
   const [events, setEvents] = React.useState([]);
 
   const setUtcDate = (dateStr) => {
@@ -27,29 +26,29 @@ function App() {
 
   const convertEvent = (item) => {
     let eventObj = {};
-    eventObj['id'] = item['uid'];
-    eventObj['title'] = item['title'];
-    eventObj['notes'] = item['description'];
-    eventObj['type'] = item['eventType'];
-    
+    eventObj["id"] = item["uid"];
+    eventObj["title"] = item["title"];
+    eventObj["notes"] = item["description"];
+    eventObj["type"] = item["eventType"];
+
     // For now you can't have a endTime on a different day as startTime
-    const utcStart = setUtcDate(item['startTime']);
-    const utcEnd = setUtcDate(item['endTime']);
-    eventObj['year'] = utcStart.getFullYear();
-    eventObj['month'] = utcStart.getMonth();
-    eventObj['day'] = utcStart.getDate();
-    eventObj['startTime'] = `${utcStart.getHours()}:${utcStart.getMinutes()}`;
-    eventObj['endTime'] = `${utcEnd.getHours()}:${utcEnd.getMinutes()}`;
-    eventObj['slug'] = item['slug'];
+    const utcStart = setUtcDate(item["startTime"]);
+    const utcEnd = setUtcDate(item["endTime"]);
+    eventObj["year"] = utcStart.getFullYear();
+    eventObj["month"] = utcStart.getMonth();
+    eventObj["day"] = utcStart.getDate();
+    eventObj["startTime"] = `${utcStart.getHours()}:${utcStart.getMinutes()}`;
+    eventObj["endTime"] = `${utcEnd.getHours()}:${utcEnd.getMinutes()}`;
+    eventObj["slug"] = item["slug"];
     return eventObj;
-  }
+  };
 
   React.useEffect(() => {
     const fetchEvents = async () => {
-      const res = await fetch(`${url}/events/`);
+      const res = await fetch(`${url}/api/events/`);
       const jsonData = await res.json();
       // Convert to expected format
-      const data = jsonData.map(item => { 
+      const data = jsonData.map((item) => {
         return convertEvent(item);
       });
 
@@ -60,23 +59,42 @@ function App() {
   }, []);
 
   const addEvent = async (event) => {
-    const startHour = event['startTime'].slice(0, event['startTime'].indexOf(':'));
-    const startMinute = event['startTime'].slice(-2);
-    const endHour = event['endTime'].slice(0, event['endTime'].indexOf(':'));
-    const endMinute = event['endTime'].slice(-2);
-    const startDate = new Date(event['year'], event['month'], event['day'], startHour, startMinute);
-    const endDate = new Date(event['year'], event['month'], event['day'], endHour, endMinute);
+    const startHour = event["startTime"].slice(
+      0,
+      event["startTime"].indexOf(":")
+    );
+    const startMinute = event["startTime"].slice(-2);
+    const endHour = event["endTime"].slice(0, event["endTime"].indexOf(":"));
+    const endMinute = event["endTime"].slice(-2);
+    const startDate = new Date(
+      event["year"],
+      event["month"],
+      event["day"],
+      startHour,
+      startMinute
+    );
+    const endDate = new Date(
+      event["year"],
+      event["month"],
+      event["day"],
+      endHour,
+      endMinute
+    );
     const postEvent = {
-      'title': event['title'],
-      'startTime': `${startDate.getUTCFullYear()}-${startDate.getUTCMonth() + 1}-${startDate.getUTCDate()}T${startDate.getUTCHours()}:${startDate.getUTCMinutes()}:${startDate.getUTCSeconds()}`,
-      'endTime': `${endDate.getUTCFullYear()}-${endDate.getUTCMonth() + 1}-${endDate.getUTCDate()}T${endDate.getUTCHours()}:${endDate.getUTCMinutes()}:${endDate.getUTCSeconds()}`,
-      'description': event['notes'],
+      title: event["title"],
+      startTime: `${startDate.getUTCFullYear()}-${
+        startDate.getUTCMonth() + 1
+      }-${startDate.getUTCDate()}T${startDate.getUTCHours()}:${startDate.getUTCMinutes()}:${startDate.getUTCSeconds()}`,
+      endTime: `${endDate.getUTCFullYear()}-${
+        endDate.getUTCMonth() + 1
+      }-${endDate.getUTCDate()}T${endDate.getUTCHours()}:${endDate.getUTCMinutes()}:${endDate.getUTCSeconds()}`,
+      description: event["notes"],
     };
 
-    const res = await fetch(`${url}/events/`, {
-      method: 'POST',
+    const res = await fetch(`${url}/api/events/`, {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
       body: JSON.stringify(postEvent),
     });
@@ -90,13 +108,19 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path='/addEvent' element={<AddEvent onAdd={addEvent} />} />
-        <Route path='/' element={
-          <>
-            <Calendar displayDate={new Date()} events={events} />
-          </>
-        } />
-        <Route path='/event/:slug' element={<EventView url={url} convertEvent={convertEvent} />} />
+        <Route path="/addEvent" element={<AddEvent onAdd={addEvent} />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Calendar displayDate={new Date()} events={events} />
+            </>
+          }
+        />
+        <Route
+          path="/event/:slug"
+          element={<EventView url={url} convertEvent={convertEvent} />}
+        />
       </Routes>
     </Router>
   );
